aa160f04ff2fb4770dcd2e9f6e0e989cf8ff123e
Jay Mehta <mehta.x.jay@gsk.com>
2022-03-15 14:02:16 +0530

    Continued Test

--- mytest/main/default/lwc/helloWorld/helloWorld.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class HelloWorld extends LightningElement {
-  greeting = 'THIS WILL BE A NEW BRANCH WHICH WILL BE PULLED TO THE RBD AND CHERRYPICKED FURTHER ON';
-  changeHandler(event) {
-    this.greeting = event.target.value;
-  }
-}
